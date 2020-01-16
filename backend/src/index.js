d33bacf13e3c6e03a1ac2bd04fda9e7b101805db
VHPV-ASUS-2\vitor <vitorvilela@sapo.pt>
2020-01-16 23:48:45 +0000

    Aula 1 (initial commit)

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.set('useCreateIndex', true);
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-7imyk.mongodb.net/week10?retryWrites=true&w=majority',{
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+//Tipos de parâmetros:
+
+// Query params: request.query (Filtros, ordenação, paginação, ...)
+// Rout params: request.params (Identificar um recurso na alteração ou remoção)
+// Body params:
+
+
+app.listen(3333);
